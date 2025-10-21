d700c17eeb209223f411ee3d29fbfaade1feb69b
Gavin <lodorian18@gmail.com>
2025-10-21 17:26:24 +0800

    meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Suspense } from "react";
+import Script from "next/script"; // 1. Import next/script
 import "./globals.css";
 import { CookieConsent } from "@/components/cookie-consent";
 import { ScrollToTop } from "@/components/scroll-to-top";
@@ -52,10 +53,40 @@ export default function RootLayout({
         <meta name="twitter:image" content="/logo.png" />
       </head>
       <body className={`${montserrat.variable} font-sans antialiased`}>
+        {/* 2. Add the <noscript> tag right after <body> */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=710925395352302&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <ScrollToTop />
         <Suspense fallback={null}>{children}</Suspense>
         <CookieConsent />
         <Analytics />
+
+        {/* 3. Add the main <Script> component at the end of <body> */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '710925395352302');
+                fbq('track', 'PageView');
+              `,
+          }}
+        />
       </body>
     </html>
   );
