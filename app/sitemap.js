55fa8be8c86e0e1eaaa7fd5a6432874725e53342
devdekodecamp <dev@dekodecamp.com>
2025-11-09 19:06:42 +0800

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,35 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://dekodecamp.com/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://dekodecamp.com/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.dekodecamp.com/programs/salesforce-adm201',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.dekodecamp.com/programs/salesforce-developer',
+      lastModified: new Date(),
+    {
+      url: 'https://www.dekodecamp.com/programs/full-stack-developer',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.dekodecamp.com/programs/career-pathways',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.dekodecamp.com/blogs',
+      lastModified: new Date(),
+    },
+        {
+      url: 'https://www.dekodecamp.com/contact',
+      lastModified: new Date(),
+    },
+  ];
+}
